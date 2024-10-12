e1be2bd53fac41986bc6872e1fda562cedcca8e4
omnitrix21 <okand7946@gmail.com>
2024-10-12 10:34:11 +0530

    reduce redundancy
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

---
+++ blog-data.js
@@ -0,0 +1,4 @@
+const blogPostsMetadata = [
+    { id: "002", title: "Are Custom ROMs Becoming Irrelevant?", date: "2024-10-11", author: "AARAV", file: "post_002.md" },
+    { id: "001", title: "How to Install Linux on a Windows PC: A Beginner's Guide", date: "2024-10-10", author: "AARAV", file: "post_001.md" },
+];
